5db756cb34e5624cff6669191fdd449a289233af
ハジメ <117750380+MAIKIW@users.noreply.github.com>
2023-11-03 15:38:57 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,22 +1,20 @@
-local function onPromptTriggered(prompt)
-    local player = game.Players:GetPlayerFromCharacter(prompt.Parent)
-    if player then
-        local character = player.Character
-        if character and not character:FindFirstChild("Seat") then
+function onKeyPress(input)
+    if input.KeyCode == Enum.KeyCode.E then
+        if not character:FindFirstChild("Seat") then
             -- สร้าง Seat เพื่อนั่ง
             local seat = Instance.new("Seat")
             seat.Parent = workspace
             seat.CFrame = character.HumanoidRootPart.CFrame * CFrame.new(0, -2, 0) -- ปรับตำแหน่งของ Seat
             character:MoveTo(seat.Position)
             character.Humanoid.Sit = true -- เปลี่ยนสถานะให้นั่ง
+        else
+            -- ลบ Seat เมื่อต้องการยืนขึ้น
+            character:MoveTo(character.HumanoidRootPart.Position + Vector3.new(0, 2, 0)) -- ปรับตำแหน่งของตัวละคร
+            character.Humanoid.Sit = false -- เปลี่ยนสถานะให้ยืน
+            character.Seat:Destroy()
         end
     end
 end
 
--- สร้าง ProximityPrompt
-local proximityPrompt = Instance.new("ProximityPrompt")
-proximityPrompt.ActionText = "Press E to Sit"
-proximityPrompt.ObjectText = "Seat"
-proximityPrompt.Parent = workspace -- นำ ProximityPrompt มาใส่ใน workspace หรือเป็นส่วนหนึ่งของตัวละคร
-
-proximityPrompt.Triggered:Connect(onPromptTriggered)
+-- เมื่อผู้เล่นกดปุ่ม
+UserInputService.InputBegan:Connect(onKeyPress)
